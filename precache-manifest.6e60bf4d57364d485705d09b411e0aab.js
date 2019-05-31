30418481b86f894171277ca2013646012506951d
Faneste <faneste12345@gmail.com>
2019-05-31 02:18:14 +0200

    Updates

---
+++ precache-manifest.6e60bf4d57364d485705d09b411e0aab.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817e008c690694c10488eac7adbc0892",
+    "url": "/TV90tih/index.html"
+  },
+  {
+    "revision": "715be7deb506d3d9a806",
+    "url": "/TV90tih/static/css/main.8524836d.chunk.css"
+  },
+  {
+    "revision": "8b1cca6b778ceeb29423",
+    "url": "/TV90tih/static/js/2.151afd9b.chunk.js"
+  },
+  {
+    "revision": "715be7deb506d3d9a806",
+    "url": "/TV90tih/static/js/main.a7b0b23a.chunk.js"
+  },
+  {
+    "revision": "b034c9f9d3d893b133d8",
+    "url": "/TV90tih/static/js/runtime~main.d7c32510.js"
+  },
+  {
+    "revision": "52d1d3d73e38fa79b0421e08cc074385",
+    "url": "/TV90tih/static/media/backgroundDefault.52d1d3d7.jpg"
+  },
+  {
+    "revision": "4923ec9444bd37e7fb4dff8827f9bd90",
+    "url": "/TV90tih/static/media/backgroundOne.4923ec94.jpg"
+  },
+  {
+    "revision": "7fe49646c494bf7ee14117cd4dd32950",
+    "url": "/TV90tih/static/media/backgroundTwo.7fe49646.jpg"
+  },
+  {
+    "revision": "6219c2204a93f6d2c39fda5577e2a3bd",
+    "url": "/TV90tih/static/media/changeChannelButtonImage.6219c220.png"
+  },
+  {
+    "revision": "f9f41cd93ca2948dab8c3a76a128634d",
+    "url": "/TV90tih/static/media/eiNisLogo.f9f41cd9.png"
+  },
+  {
+    "revision": "65cb344c64e3d29ae35396789de64c2e",
+    "url": "/TV90tih/static/media/pixelFont.65cb344c.ttf"
+  },
+  {
+    "revision": "c2b06483e9125d91fbed2e1b3745d990",
+    "url": "/TV90tih/static/media/remote.c2b06483.png"
+  },
+  {
+    "revision": "646ad0cd423fe32fc962260f9b8faffc",
+    "url": "/TV90tih/static/media/staticSound.646ad0cd.mp3"
+  },
+  {
+    "revision": "d06992a5feaca654da5a3e8c68921f43",
+    "url": "/TV90tih/static/media/tv.d06992a5.png"
+  },
+  {
+    "revision": "3af1ff274827af3c1e675671c78252ff",
+    "url": "/TV90tih/static/media/tvChannelBackground.3af1ff27.png"
+  },
+  {
+    "revision": "740b6b2a3510604e3e3bba32c735d30b",
+    "url": "/TV90tih/static/media/tvNoise.740b6b2a.gif"
+  },
+  {
+    "revision": "eb812e89376352081c46f8f8de85e4b6",
+    "url": "/TV90tih/static/media/tvTutorial.eb812e89.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b59a2591545a0ebfd73964022d7636d.js"
+  "/TV90tih/precache-manifest.6e60bf4d57364d485705d09b411e0aab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TV90tih/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
